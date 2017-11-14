e4ea957e29ff06490d2e1c0ca05bd11e5f200673
Paul Boot <paulboot@gmail.com>
2017-11-14 22:06:00 +0100

    First example with a headers exchange

---
+++ amqp/emit_headers.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+# Source: http://deontologician.tumblr.com/post/19741542377/using-pika-to-create-headers-exchanges-with
+
+import pika
+import sys
+
+connection = pika.BlockingConnection(pika.ConnectionParameters(
+        host='localhost'))
+channel = connection.channel()
+
+channel.exchange_declare(exchange='testing',
+                         type='headers')
+
+fields = {}
+try:
+    while True:
+        data = raw_input('> ')
+        if '=' in data:
+            key, val = data.split('=')
+            fields[key] = val
+            continue
+        channel.basic_publish(exchange = 'testing',
+                              routing_key = '',
+                              body = data,
+                              properties = \
+                                  pika.BasicProperties(headers = fields))
+        print(' [x] Send {0} with headers: {1}'.format(data, fields))
+except KeyboardInterrupt:
+    print('Bye')
+    connection.close()
+    sys.exit(0)
---
+++ amqp/receive_headers.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+
+# Source: http://deontologician.tumblr.com/post/19741542377/using-pika-to-create-headers-exchanges-with
+
+import pika
+import sys
+
+connection = pika.BlockingConnection(pika.ConnectionParameters(
+        host='localhost'))
+channel = connection.channel()
+
+channel.exchange_declare(exchange='testing',
+                         type='headers')
+
+result = channel.queue_declare(exclusive=True)
+if not result:
+    print('Queue didnt declare properly!')
+    sys.exit(1)
+queue_name = result.method.queue
+
+
+channel.queue_bind(exchange='testing',
+                   queue = queue_name,
+                   routing_key = '',
+                   arguments = {'ham': 'good', 'x-match':'any'})
+
+def callback(ch, method, properties, body):
+    print("{headers}:{body}".format(headers = properties.headers,
+                                    body = body))
+
+channel.basic_consume(callback,
+                      queue = queue_name,
+                      no_ack=True)
+
+try:
+    channel.start_consuming()
+except KeyboardInterrupt:
+    print('Bye')
+    connection.close()
+    sys.exit(0)
