4f52f06029345a924419a8d326ec47f3663b89ab
paulboot <paulboot@gmail.com>
2017-12-01 23:54:03 +0100

    Added configuration secrey.py and json decoding, tag header filter.

--- kinetic/receive_kinetic.py
+++ kinetic/receive_kinetic.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
 # COPYRIGHT
 #     Copyright (c) 2016 by Cisco Systems, Inc.
 #     All rights reserved.
@@ -6,6 +8,7 @@
 # DESCRIPTION
 #    This is a AMQP client side message consumer programme.
 
+from secret import *
 import pika
 import time
 import sys
@@ -13,59 +16,61 @@ import ssl
 import argparse
 import datetime
 import logging
+import json
+
+def timenownano():
+    return "%18.f" % (time.time() * 10 ** 9)
 
 def callback(ch, method, properties, body):
     print(" [x] rk: {}, headers: {}, msg: {}, ts: {}".
           format(method.routing_key, properties.headers, body,
                  datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S")))
+
     print('-----------')
-    print(body)
+    dict = json.loads(str(body, 'utf-8'))
+    payload = "observations,host=gw-zh-delft01,units=" + dict['assetpi']['units'] + " " +\
+              "WaterTemperature=" + str(dict['assetpi']['temperature']) + " " +\
+              str(dict['assetpi']['timestamp'])
+    print(payload)
+    # Send to InfluxDB over HTTP
 
 def main():
     logging.basicConfig(level=logging.INFO)
     
     parser = argparse.ArgumentParser(description="AMQP client")
-    parser.add_argument("-v", "--vhost", help='AMQP virtual host name, default: IOTSP_INTERNAL', default='IOTSP_INTERNAL', dest='vhost')
-    parser.add_argument("-p", "--port", help='AMQP virtual host ssl port, default: 5671', default=5671, dest='port', type=int)
-    parser.add_argument("-u", "--user", help='AMQP user name, default: test', default='test', dest='user')
-    parser.add_argument("-P", "--password", help='AMQP user password, default: test', default='test', dest='password')
-    parser.add_argument("-e", "--exchange", help='AMQP exchange name, default: unknown', default='unknown', dest='exchange')
-    parser.add_argument("-l", "--label", help='AMQP label,  must be in format a=b, default: None', default=None, dest='label')
-    parser.add_argument("-r", "--rmqphost", help='AMQP host ip, default: 127.0.0.1', default='127.0.0.1', dest='rmqp_host')
-    parser.add_argument("-k", "--routingkey", help='AMQP routing key, default: dev2app', default='dev2app', dest='routing_key')
     parser.add_argument('--version', action='version', version='1.0')
     args = parser.parse_args()
     
-    queue_name = args.user + ':' + datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S")
-    credentials = pika.PlainCredentials(args.user, args.password)
+    queue_name = user + ':' + datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S")
+    credentials = pika.PlainCredentials(user, password)
 
     try:
         # Setup our ssl options
         ssl_options = ({"ca_certs": "cert.crt",
                        "cert_reqs": ssl.CERT_REQUIRED})
         parameters = pika.ConnectionParameters(
-                     host=args.rmqp_host, virtual_host=args.vhost, 
-                     ssl=True, ssl_options=ssl_options, credentials = credentials, port=args.port) 
+                     host=amqphost, virtual_host=vhost,
+                     ssl=True, ssl_options=ssl_options, credentials = credentials, port=port)
         connection = pika.BlockingConnection(parameters)
         channel = connection.channel()
         channel.queue_declare(queue=queue_name, passive=False, 
                               durable=False, exclusive=False, auto_delete=True) # Declare a queue
-        if args.label:
-            lbl = args.label.split('=')
-            channel.queue_bind(exchange=args.exchange, queue=queue_name, routing_key=args.routing_key,
+        if label:
+            lbl = label.split('=')
+            channel.queue_bind(exchange=exchange, queue=queue_name, routing_key=routingkey,
                             arguments = {lbl[0]:lbl[1], 'x-match':'all'})
                             #arguments = {'gatewayId': '9999148', 'x-match':'any'})
         else:
-            channel.queue_bind(exchange=args.exchange, queue=queue_name, routing_key=args.routing_key)
+            channel.queue_bind(exchange=exchange, queue=queue_name, routing_key=routingkey)
     except IndexError:
         logging.error("label format error, should be < x=y >!")
         sys.exit()
     except Exception as e:
-        logging.error(args.rmqp_host + " Connection error!")
+        logging.error(amqphost + " Connection error!")
         logging.error(" %s", e.message)
         logging.error(type(e))
         sys.exit()
-    print(' [*] Waiting for data [tags: {}]. To exit press CTRL+C'.format(args.label))
+    print(' [*] Waiting for data [tags: {}]. To exit press CTRL+C'.format(label))
     channel.basic_consume(callback,  queue=queue_name, no_ack=True)
     channel.start_consuming()
 
